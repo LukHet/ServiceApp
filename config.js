068edb725ce866e141ce49e03fbf863b114527ae
LukHet <lukaszhetman1@gmail.com>
2024-02-24 23:44:20 +0100

    Added login, admin and cookies.

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  keySession: ["PROJECTKEY"],
+  maxAgeSession: 24 * 60 * 60 * 1000,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const login = "login";
+const password = "password";
 
 /* GET home page. */
 router.get("/", (req, res) => {
   res.render("index", { title: "Express" });
+});
+
+router.get("/login", (req, res) => {
+  res.render("login", { title: "Logowanie" });
+});
+
+router.post("/login", (req, res) => {
+  if (req.body.login === login && req.body.password === password) {
+    req.session.admin = 1;
+    res.redirect("/admin");
+    return;
+  }
+  res.redirect("/login");
 });
 
 module.exports = router;
