28d4e102ae2581ad2700e2580f02c300ce600835
LukHet <lukaszhetman1@gmail.com>
2024-03-04 00:54:02 +0100

    Added deleting and displaying data from databse on front-end side

--- routes/admin.js
+++ routes/admin.js
@@ -11,22 +11,46 @@ router.all("*", (req, res, next) => {
   next();
 });
 
+const getItemsFromDB = async () => {
+  const itemsNews = await News.find({});
+  return itemsNews;
+};
+
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.render("admin/index", { title: "Admin" });
+  console.log("home");
+  getItemsFromDB()
+    .then((data) => {
+      console.log(data);
+      res.render("admin/index", { title: "Admin", data });
+    })
+    .catch((err) => console.log(err));
 });
 
 router.get("/news/add", (req, res) => {
-  res.render("admin/news-form", { title: "Dodaj news" });
+  res.render("admin/news-form", { title: "Dodaj news", errors: {}, body: {} });
 });
 
 router.post("/news/add", (req, res) => {
   const body = req.body;
   const newsData = new News(body);
   const errors = newsData.validateSync();
+  newsData
+    .save()
+    .then((data) => {
+      console.log(data);
+      res.redirect("/admin");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("admin/news-form", { title: "Dodaj news", errors, body });
+    });
+});
 
-  newsData.save();
-  res.render("admin/news-form", { title: "Dodaj news", errors });
+router.get("/news/delete/:id", (req, res) => {
+  News.findByIdAndDelete(req.params.id)
+    .then(() => res.redirect("/admin"))
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
