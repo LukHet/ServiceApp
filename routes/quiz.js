fbddfa6af2bc764b5b7ff01eb2398a38466d7119
LukHet <lukaszhetman1@gmail.com>
2024-03-13 19:09:03 +0100

    Finished project.

--- routes/quiz.js
+++ routes/quiz.js
@@ -4,8 +4,27 @@ const Quiz = require("../models/quiz");
 
 /* GET home page. */
 router.get("/", (req, res) => {
+  const show = !req.session.vote;
   Quiz.find({})
-    .then((data) => res.render("quiz", { title: "Quiz", data }))
+    .then((data) => {
+      let sum = 0;
+      data.forEach((item) => {
+        sum += item.vote;
+      });
+      res.render("quiz", { title: "Quiz", data, show, sum });
+    })
+    .catch((error) => console.log(error));
+});
+
+router.post("/", (req, res) => {
+  const body = req.body.quiz;
+  Quiz.findOne({ _id: body })
+    .then((data) => {
+      data.vote += 1;
+      data.save();
+      req.session.vote = 1;
+      res.redirect("/quiz");
+    })
     .catch((error) => console.log(error));
 });
 
